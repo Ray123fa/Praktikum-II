ad01aaade2f2202a163082b23906510424119b11
Rayhan F <rayhanfrdh123@gmail.com>
2024-09-26 21:51:23 +0700

    Initial commit PPK

---
+++ prak1/src/main/java/com/example/prak1/Prak1Application.java
@@ -0,0 +1,13 @@
+package com.example.prak1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prak1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prak1Application.class, args);
+	}
+
+}
---
+++ prak1/src/test/java/com/example/prak1/Prak1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prak1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prak1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

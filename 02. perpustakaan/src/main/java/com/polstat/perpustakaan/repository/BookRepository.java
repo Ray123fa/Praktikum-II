147c34d0447e27c48b81020c17b63caa8bb84fd6
Rayhan F <rayhanfrdh123@gmail.com>
2024-10-09 17:50:22 +0700

    Refactor inetrface repository

--- 02. perpustakaan/src/main/java/com/polstat/perpustakaan/repository/BookRepository.java
+++ 02. perpustakaan/src/main/java/com/polstat/perpustakaan/repository/BookRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long>{
-    List<Book> findByTitleContainingIgnoreCase(String keyword);
-    List<Book> findByAuthorContainingIgnoreCase (String keyword);
     List<Book> findByAuthorContainingOrTitleContaining(String author, String title);
 }
--- 02. perpustakaan/src/main/java/com/polstat/perpustakaan/service/BookServiceImpl.java
+++ 02. perpustakaan/src/main/java/com/polstat/perpustakaan/service/BookServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class BookServiceImpl implements BookService{
+public class BookServiceImpl implements BookService {
     @Autowired
     private BookRepository bookRepository;
 
